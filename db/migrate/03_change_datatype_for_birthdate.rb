f7e487a7eec9c33925c1bb057af2bb6e288079c9
Parker Catalano <catalano.work@gmail.com>
2018-01-15 21:02:01 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthday < ActiveRecord::Migration
+class ChangeDataTypeForBirthday < ActiveRecord::Migration
 
   def change
     change_column :students, :birthdate, :datetime
